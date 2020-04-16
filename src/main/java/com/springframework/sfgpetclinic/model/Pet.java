9f761effa7018d4e91d848c892e407b51dc099c1
surya <vamshi.suryapally@gmail.com>
2020-04-16 18:15:32 -0400

    adding POJOs. Closes #2

---
+++ src/main/java/com/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package com.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate brithDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBrithDate() {
+        return brithDate;
+    }
+
+    public void setBrithDate(LocalDate brithDate) {
+        this.brithDate = brithDate;
+    }
+}
---
+++ src/main/java/com/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
